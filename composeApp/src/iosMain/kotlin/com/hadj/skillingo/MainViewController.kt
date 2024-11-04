2b90921a59c009843c5fcc18c721a6eb198e71d1
yet <ghg00rus300@gmail.com>
2024-11-04 15:46:07 +0400

    remove compose UI from ios app

--- composeApp/src/iosMain/kotlin/com/hadj/skillingo/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/hadj/skillingo/MainViewController.kt
@@ -1,12 +1,9 @@
 package com.hadj.skillingo
 
-import androidx.compose.ui.window.ComposeUIViewController
-import com.hadj.skillingo.di.initKoin
-
-fun MainViewController() = ComposeUIViewController(
-    configure = {
-        initKoin()
-    }
-) {
-    App()
-}
+//fun MainViewController() = ComposeUIViewController(
+//    configure = {
+//        initKoin()
+//    }
+//) {
+//    App()
+//}
