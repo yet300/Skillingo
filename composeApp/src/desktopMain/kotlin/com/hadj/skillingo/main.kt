2f7d3f293b5912b9af959ebf214c06e1aaf5bd39
yet <ghg00rus300@gmail.com>
2024-11-15 15:07:56 +0400

    feat: init Decompose for desktop
    Initialized Decompose for desktop navigation and state management.
    
    - Created a `DefaultRootComponent` to manage the root component.
    - Integrated LifecycleRegistry for managing the component lifecycle.
    - Updated the `main` function to use the `DefaultRootComponent` and handle lifecycle events.
    - Added `runOnUiThread` helper function to ensure UI operations are executed on the UI thread.

--- composeApp/src/desktopMain/kotlin/com/hadj/skillingo/main.kt
+++ composeApp/src/desktopMain/kotlin/com/hadj/skillingo/main.kt
@@ -2,12 +2,47 @@ package com.hadj.skillingo
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import com.arkivanov.decompose.DefaultComponentContext
+import com.arkivanov.essenty.lifecycle.LifecycleRegistry
+import com.hadj.skillingo.root.DefaultRootComponent
+import javax.swing.SwingUtilities
 
 fun main() = application {
+//    val size = calculateWindowSizeClass()
+
+    val lifecycle = LifecycleRegistry()
+    val root = runOnUiThread {
+        DefaultRootComponent(
+            componentContext = DefaultComponentContext(lifecycle = lifecycle),
+        )
+    }
+
     Window(
         onCloseRequest = ::exitApplication,
         title = "Skillingo",
     ) {
-        App()
+        App(root)
     }
+}
+
+internal fun <T> runOnUiThread(block: () -> T): T {
+    if (SwingUtilities.isEventDispatchThread()) {
+        return block()
+    }
+
+    var error: Throwable? = null
+    var result: T? = null
+
+    SwingUtilities.invokeAndWait {
+        try {
+            result = block()
+        } catch (e: Throwable) {
+            error = e
+        }
+    }
+
+    error?.also { throw it }
+
+    @Suppress("UNCHECKED_CAST")
+    return result as T
 }
