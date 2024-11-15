e0bd845ede2b29fcc6d5824114b93846b21537dc
yet <ghg00rus300@gmail.com>
2024-11-15 14:51:13 +0400

    feat: Implement theme mode changing in OnBoarding screen
    - Added the ability to change the theme mode in the OnBoarding screen.
    - Integrated the ThemeModeRepository to persist the selected theme mode.
    - Updated the OnBoardingComponent with a model to hold the current theme mode.
    - Implemented the changeThemeMode function to update the theme and persist it.
    - Initialized the OnBoardingComponent with the current theme mode from the repository.

--- shared/feature/onboarding/src/commonMain/kotlin/com/hadj/skillingo/onboarding/DefaultOnBoardingComponent.kt
+++ shared/feature/onboarding/src/commonMain/kotlin/com/hadj/skillingo/onboarding/DefaultOnBoardingComponent.kt
@@ -1,11 +1,44 @@
 package com.hadj.skillingo.onboarding
 
 import com.arkivanov.decompose.ComponentContext
+import com.arkivanov.decompose.value.MutableValue
+import com.arkivanov.decompose.value.Value
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+import ru.hadj.skillingo.common.AppDispatchers
+import ru.hadj.skillingo.domain.model.ThemeMode
+import ru.hadj.skillingo.domain.repository.ThemeModeRepository
 
 class DefaultOnBoardingComponent(
     private val componentContext: ComponentContext,
     private val onNavigate: () -> Unit
-) : OnBoardingComponent, ComponentContext by componentContext {
+) : OnBoardingComponent, KoinComponent, ComponentContext by componentContext {
+
+    private val themeModeRepository by inject<ThemeModeRepository>()
+    private val dispatchers by inject<AppDispatchers>()
+
+    private val _model = MutableValue(
+        OnBoardingComponent.Model()
+    )
+    override val model: Value<OnBoardingComponent.Model> = _model
+
+    init {
+        CoroutineScope(dispatchers.io).launch {
+            themeModeRepository.readThemeMode().collect { themeMode ->
+                _model.value = _model.value.copy(currentThemeMode = themeMode)
+            }
+        }
+    }
+
+    override suspend fun changeThemeMode(themeMode: ThemeMode) {
+        withContext(dispatchers.io) {
+            themeModeRepository.saveThemeMode(themeMode)
+            _model.value = _model.value.copy(currentThemeMode = themeMode)
+        }
+    }
 
     override fun onClick() = onNavigate()
 
--- shared/feature/onboarding/src/commonMain/kotlin/com/hadj/skillingo/onboarding/OnBoardingComponent.kt
+++ shared/feature/onboarding/src/commonMain/kotlin/com/hadj/skillingo/onboarding/OnBoardingComponent.kt
@@ -1,6 +1,15 @@
 package com.hadj.skillingo.onboarding
 
+import com.arkivanov.decompose.value.Value
+import ru.hadj.skillingo.domain.model.ThemeMode
+
 interface OnBoardingComponent {
+    val model: Value<Model>
+    suspend fun changeThemeMode(themeMode: ThemeMode)
 
     fun onClick()
+
+    data class Model(
+        val currentThemeMode: ThemeMode = ThemeMode.SYSTEM
+    )
 }
