ab6c2f6e2c0c7f99513960414dafde131f588ea7
yet <ghg00rus300@gmail.com>
2024-11-17 16:41:30 +0400

    feat: add PomodoroDao
    Added `PomodoroDao` for accessing and managing Pomodoro timer data in the database.
    
    - Implemented functions to get, insert, update, and delete Pomodoro timer entries.
    - Uses Kotlin coroutines and Flow for asynchronous operations and data observation.
    - Annotations like `@Dao`, `@Query`, `@Insert`, `@Update`, and `@Delete` are used for database interactions.

---
+++ core/database/src/commonMain/kotlin/ru/hadj/database/dao/PomodoroDao.kt
@@ -0,0 +1,24 @@
+package ru.hadj.database.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+import ru.hadj.database.model.PomodoroTimerDBO
+
+@Dao
+interface PomodoroDao {
+    @Query("SELECT * FROM pomodoro_timer WHERE id = :id")
+    fun getPomodoroTimer(id: Long = 0): Flow<PomodoroTimerDBO?>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertPomodoroTimer(timer: PomodoroTimerDBO)
+
+    @Update
+    suspend fun updatePomodoroTimer(timer: PomodoroTimerDBO)
+
+    @Query("DELETE FROM pomodoro_timer WHERE id = :id")
+    suspend fun deletePomodoroTimer(id: Long = 0)
+} 
